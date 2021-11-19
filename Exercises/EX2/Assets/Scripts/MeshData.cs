cc4160c1e3b531427d9c66d39fc6541848d9f81f
ucohen <8166310+ucohen@users.noreply.github.com>
2021-11-19 10:05:29 +0200

    restructure project
    Signed-off-by: ucohen <8166310+ucohen@users.noreply.github.com>

---
+++ Exercises/EX2/Assets/Scripts/MeshData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MeshData
+{
+    public List<Vector3> vertices; // The vertices of the mesh 
+    public List<int> triangles; // Indices of vertices that make up the mesh faces
+    public Vector3[] normals; // The normals of the mesh, one per vertex
+
+    // Class initializer
+    public MeshData()
+    {
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+    }
+
+    // Returns a Unity Mesh of this MeshData that can be rendered
+    public Mesh ToUnityMesh()
+    {
+        Mesh mesh = new Mesh
+        {
+            vertices = vertices.ToArray(),
+            triangles = triangles.ToArray(),
+            normals = normals
+        };
+
+        return mesh;
+    }
+
+    // Calculates surface normals for each vertex, according to face orientation
+    public void CalculateNormals()
+    {
+        // Your implementation
+    }
+
+    // Edits mesh such that each face has a unique set of 3 vertices
+    public void MakeFlatShaded()
+    {
+        // Your implementation
+    }
+}
